362374aef87eafe16e6ef5ee5f8eef710d694caa
HyunJiYe <66548781+HyunJiYe@users.noreply.github.com>
2020-12-13 00:51:53 +0900

    Create whilelab.c

---
+++ code/LAB/whilelab.c
@@ -0,0 +1,18 @@
+/* 20200824 현지예
+   file : whilelab.c */
+
+#include <stdio.h>
+#define MAX 20
+
+int main(void)
+{
+	int n = 0;
+
+	while ((n <= MAX)) {
+		printf("%4d", n);
+		n += 3;
+	}
+	puts("");
+
+	return 0;
+}
