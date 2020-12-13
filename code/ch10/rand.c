4f506c0fa44e827c688e9b072bc44890269f6333
HyunJiYe <66548781+HyunJiYe@users.noreply.github.com>
2020-12-13 18:03:11 +0900

    Create rand.c

---
+++ code/ch10/rand.c
@@ -0,0 +1,16 @@
+/* 20200824 현지예
+   file : rand.c */
+
+#include <stdio.h>
+
+#include <stdlib.h>  //rand()위한 해더파일 포함
+
+int main(void)
+{
+	printf("0~%5d 사이의 난수 5개: rand()\n", RAND_MAX);
+	for (int i = 0; i < 5; i++)
+		printf("%5d ", rand());
+	puts("");
+
+	return 0;
+}
